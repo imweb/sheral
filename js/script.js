62bda92b04587cd08f57799443b9ae52dffbfbc1
marvin1023 <xuyc_brother@foxmail.com>
2016-05-28 12:51:40 +0800

    add demo html

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', function(){
+// 	var Back = document.querySelectorAll('.js-back')[0];
+// 	Back.addEventListener('click', function(){
+// 		window.location.href = '/' ;
+// 	})
+
+// 	// toast
+
+// })
+
+$(function() {
+	$('.js-back').on('click', function() {
+		window.location.href = '/' ;
+	})
+	// $('.js-toast-btn').on('click', function() {
+	// 	$('.js-toast-wrap').addClass('active');
+	// })
+})
+
